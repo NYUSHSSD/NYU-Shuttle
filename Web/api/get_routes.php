945a3562539519de3bb004877e7948314e29d5b1
xu.li <AthenaLightenedMyPath@gmail.com>
2015-09-11 14:16:31 +0800

    update API

--- Web/api/get_routes.php
+++ Web/api/get_routes.php
@@ -1,17 +1,38 @@
 <?php
 include 'db_helper.php';
 
-$sql = "select * from schedule";
-$rst = db_helper::query($sql);
+$result = db_helper::query("select * from schedule");
 
 $routes = array();
-if ($rst) {
-    foreach($rst as $row) {
+if ($result) {
+    foreach($result as $row) {
         $route = $row['route'];
         $route = explode("-", $route);
         $route = $route[0];
 
-        $routes[$route][] = $row;
+        if (!isset($routes[$route])) {
+            $routes[$route] = array(
+                'code'      => $route,
+                'schedules' => array(),
+                'from'      => $row['going_from'],
+                'to'        => $row['going_to'],
+            );
+        }
+
+        $routes[$route]['schedules'][] = array(
+            'id'        => $row['route'],
+            'departure' => $row['departure'],
+            'arrival'   => $row['arrival'],
+            'weekdays'  => array(
+                filter_var($row['sunday'], FILTER_VALIDATE_BOOLEAN),
+                filter_var($row['monday'], FILTER_VALIDATE_BOOLEAN),
+                filter_var($row['tuesday'], FILTER_VALIDATE_BOOLEAN),
+                filter_var($row['wednesday'], FILTER_VALIDATE_BOOLEAN),
+                filter_var($row['thursday'], FILTER_VALIDATE_BOOLEAN),
+                filter_var($row['friday'], FILTER_VALIDATE_BOOLEAN),
+                filter_var($row['saturday'], FILTER_VALIDATE_BOOLEAN),
+            )
+        );
     }
 }
 
